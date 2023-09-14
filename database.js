96b9d14721ca3c9e9da5f24bd020dc62d1e11366
kinqie <albert.rick.10.04@gmail.com>
2023-09-14 23:48:15 +0700

    delete trash text

--- database.js
+++
@@ -1,4 +0,0 @@
-const FrenchVerbs = require("french-verbs");
-const Lefff = require("french-verbs-lefff/dist/conjugations.json");
-
-console.log("je " + FrenchVerbs.getConjugation(Lefff, "finir", "PRESENT", 0));
